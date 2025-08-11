3d4326ef27976f4bcefcc95239e04aaae694b8d2
Yisel <yiselorenasalzar@gmail.com>
2025-08-11 16:47:52 -0500

    tenant

--- backOrmPro/start/routes/BlogRoutes.ts
+++
@@ -1,7 +0,0 @@
-import Route from "@adonisjs/core/services/router"
-import BlogController from "../../app/controller/PublicacionBlogController.js"
-
-const blog = new BlogController()
-
-Route.post('/crearBlog', blog.crearBlog)
-Route.get('/listarBlog', blog.listarBlog)
